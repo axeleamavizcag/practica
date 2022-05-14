8d974f8a926f58c9094590f33c9d17c91d610369
axeleamavizcag <102840661+axeleamavizcag@users.noreply.github.com>
2022-05-13 20:08:56 -0700

    Se agrego practica3

---
+++ nombre.py
@@ -0,0 +1,6 @@
+nombre= str(input("Ingresa primer nombre: "))
+nombre2= str(input("Ingresa segundo nombre: "))
+ap1= str(input("Ingresa primer apellido: "))
+ap2= str(input("ingresa segundo apellido: "))
+
+print("Mi nombre completo es: ", nombre + " " + nombre2 + " " + ap1 + " " + ap2)
